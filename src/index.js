42677610c857c0a537a876ef33339751b696572c
devsh0 <devashishjaiswal86@gmail.com>
2021-10-08 09:44:06 +0530

    Expose Player state to children
    The necessary next step to make the exposed state read-only outside the
    Player.

--- src/index.js
+++ src/index.js
@@ -2,15 +2,23 @@ import React, {Fragment} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+const PlayerState = React.createContext(null);
+
 class SeekSlider extends React.Component {
+    static contextType = PlayerState;
+
     constructor(props) {
         super(props);
+    }
+
+    componentDidMount = () => {
+        console.log(this.context);
     }
 
     render() {
         return (
             <div className={"seek-slider"}>
-                <input type="range" min="1" max="100" step="1" />
+                <input type="range" min="1" max="100" step="1"/>
             </div>
         );
     }
@@ -40,56 +48,61 @@ class PlayButton extends React.Component {
 class Player extends React.Component {
     constructor(props) {
         super(props);
-        this.context = null
-        this.bufferSource = null
+        this.state = {
+            context: new AudioContext(),
+            bufferSource: null
+        }
+    }
+
+    loadAudio = async () => {
+        const response = await fetch("./kda.mp3");
+        let arrayBuffer = null;
+        if (response.status === 200)
+            arrayBuffer = await response.arrayBuffer();
+
+        const state = this.state;
+        const bufferSource = state.context.createBufferSource();
+        bufferSource.buffer = await state.context.decodeAudioData(arrayBuffer);
+        bufferSource.connect(state.context.destination);
+        bufferSource.start(0);
+        this.setState({bufferSource: bufferSource});
     }
 
     componentDidMount = () => {
-        this.context = new AudioContext();
-        this.bufferSource = this.context.createBufferSource();
-        fetch("./kda.mp3").then((response) => {
-            if (response.status === 200) {
-                console.log("Audio data loaded!");
-                return response.arrayBuffer();
-            }
-            throw Error("something went horribly wrong!");
-        }).then((buffer) => {
-            return this.context.decodeAudioData(buffer);
-        }).then((decodedData) => {
-            this.bufferSource.buffer = decodedData;
-            this.bufferSource.connect(this.context.destination);
-            this.bufferSource.start(0);
-        }).catch((error) => {
-            console.log(error.message);
-        });
+        this.loadAudio().catch((error) => {
+            console.log(error);
+        })
     }
 
     componentWillUnmount = () => {
-        this.context.close();
+        this.state.context.close();
     }
 
     togglePlayback = () => {
-        console.log(`Song Length: ${this.bufferSource.buffer.duration}`);
-        console.log(`Played Duration: ${this.context.currentTime}`);
-        
-        if (this.context.state === 'suspended') {
-            this.context.resume();
-        }
-        if (this.context.state === 'running') {
-            this.context.suspend();
-        }
+        this.setState((state) => {
+            const context = state.context;
+            if (context.state === 'suspended') {
+                context.resume();
+                return;
+            }
+            context.suspend();
+            return {context: context};
+        })
     }
 
     render() {
+        if (this.state.bufferSource === null)
+            return <span>Loading...</span>;
+
         return (
-            <Fragment>
-                <SeekSlider />
-                <PlayButton onPlaybackToggleRequested={this.togglePlayback}/>
-            </Fragment>
+            <PlayerState.Provider value={this.state}>
+                <Fragment>
+                    <SeekSlider/>
+                    <PlayButton onPlaybackToggleRequested={this.togglePlayback}/>
+                </Fragment>
+            </PlayerState.Provider>
         )
     };
 }
 
-ReactDOM.render(
-    <Player/>
-    , document.getElementById('root'));
+ReactDOM.render(<Player/>, document.getElementById('root'));
