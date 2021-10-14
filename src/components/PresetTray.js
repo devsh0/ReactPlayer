fb49a918f9c6444180626c2d900e19ae612f5a7e
devsh0 <devashishjaiswal86@gmail.com>
2021-10-14 09:48:41 +0530

    Add the ability to switch between presets

--- src/components/PresetTray.js
+++ src/components/PresetTray.js
@@ -1,14 +1,35 @@
-export default function PresetTray({show}) {
+import {useRef, useState} from "react";
+
+export default function PresetTray() {
+    const [isDropped, setDropped] = useState(false);
+    const selected = useRef();
+
+    const handleDropdownToggle = () => setDropped(!isDropped);
+
+    const handlePresetChange= (event) => {
+        selected.current.classList.remove('selected');
+        const target = event.target;
+        target.classList.add('selected');
+        selected.current = target;
+        setDropped(false);
+    }
+
     return (
-        <div className={`preset-tray ${show || 'hidden'}`}>
-            <div className={'item'}>Custom</div>
-            <div className={'item'}>Bass</div>
-            <div className={'item'}>Treble</div>
-            <div className={'item selected'}>Pop</div>
-            <div className={'item'}>Rock</div>
-            <div className={'item'}>Jazz</div>
-            <div className={'item'}>Folk</div>
-            <div className={'item'}>Party</div>
+        <div className={'drop-down-container'}>
+            <button className={'btn drop-down'}
+                    onClick={handleDropdownToggle}>
+                {selected.current ? selected.current.innerText : 'Custom'}
+            </button>
+            <div className={`preset-tray ${isDropped || 'hidden'}`}>
+                <div ref={selected} className={'item selected'} onClick={handlePresetChange}>Custom</div>
+                <div className={'item'} onClick={handlePresetChange}>Bass</div>
+                <div className={'item'} onClick={handlePresetChange}>Treble</div>
+                <div className={'item'} onClick={handlePresetChange}>Pop</div>
+                <div className={'item'} onClick={handlePresetChange}>Rock</div>
+                <div className={'item'} onClick={handlePresetChange}>Jazz</div>
+                <div className={'item'} onClick={handlePresetChange}>Folk</div>
+                <div className={'item'} onClick={handlePresetChange}>Party</div>
+            </div>
         </div>
     )
 }
